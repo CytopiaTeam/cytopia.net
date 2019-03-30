3b5989b4449982d592d0465582044c5bf7617d19
Edgar <edgar@anotherfoxguy.com>
2019-03-30 17:28:53 +0100

    :sparkles: Added openhub badge

--- config.rb
+++ config.rb
@@ -4,11 +4,13 @@
 
 configure :development do
   config[:host] = "localhost:4567"
+  config[:CDN] = "localhost:4567"
   config[:download_host] = "localhost:4567/download"
 end
 
 configure :build do
   config[:host] = "cytopia.net"
+  config[:CDN] = "cytopia.net"
   config[:download_host] = "cytopia.net/download"
 end
 
@@ -20,14 +22,13 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 
 activate :livereload
+activate :directory_indexes
 
 activate :external_pipeline,
   name: :webpack,
   command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
   source: "webpack",
   latency: 1
-
-# redirect "/repository/index.html", :to => "http://forum.cytopia.net/thread-235.html"
 
 configure :build do
   activate :minify_css
